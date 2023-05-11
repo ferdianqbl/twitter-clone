150c2750b02ee5291aa034def4a3d649a7e031e0
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-05-11 11:24:45 +0700

    create textArea height automatic

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -1,18 +1,53 @@
+import { useSession } from "next-auth/react";
 import Button from "./Button";
+import ProfileImage from "./ProfileImage";
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+
+function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
+  if (textArea == null) return;
+
+  textArea.style.height = "0px";
+  textArea.style.height = `${textArea.scrollHeight}px`;
+}
 
 export default function NewTweetForm() {
+  const session = useSession();
+  if (session.status !== "authenticated") return;
+
+  return <Form />;
+}
+
+function Form() {
+  const session = useSession();
+  const [inputValue, setInputValue] = useState("");
+  const textAreaRef = useRef<HTMLTextAreaElement>();
+  const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
+    updateTextAreaSize(textArea);
+    textAreaRef.current = textArea;
+  }, []);
+
+  useLayoutEffect(() => {
+    updateTextAreaSize(textAreaRef.current);
+  }, [inputValue]);
+
+  if (session.status !== "authenticated") return null;
+
   return (
     <form action="" className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
-        {/* <ProfileImage src="#url" /> */}
+        <ProfileImage src={session.data.user.image} />
         <textarea
+          ref={inputRef}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           name=""
           id=""
+          style={{ height: 0 }}
           placeholder="What's happening?"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </div>
-      <Button>Tweet</Button>
+      <Button className="self-end">Tweet</Button>
     </form>
   );
 }
